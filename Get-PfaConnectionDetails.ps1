c9f015de30797c928b7f382a0798b30847e3c3d5
JD Wallace <jd@jdwallace.com>
2021-01-24 17:24:38 -0800

    Show Host Written Capacity in output

--- Get-PfaConnectionDetails.ps1
+++ Get-PfaConnectionDetails.ps1
@@ -15,11 +15,9 @@ Import-Module PureStoragePowerShellSDK2
 $FAReturn = Read-Host -Prompt 'Enter the FlashArray IP or DNS entry to connect'
 $FlashArray = Connect-Pfa2Array -EndPoint $FAReturn -Credential (Get-Credential) -IgnoreCertificateError
 
-$1TB = 1024*1024*1024*1024
-
 # Create an object to store the connection details
 $ConnDetails = New-Object -TypeName System.Collections.ArrayList
-$Header = "HostName","VolumeName","LUNID","IQNs","WWNs","Provisioned(TB)"
+$Header = "HostName","VolumeName","LUNID","IQNs","WWNs","Provisioned(TB)","HostWritten(GB)"
 
 # Get Connections and filter out VVOL protocol endpoints
 $PureConns = (Get-Pfa2Connection -Array $FlashArray | Where-Object { !($_.Volume.Name  -eq "pure-protocol-endpoint") })
@@ -28,8 +26,14 @@ $PureConns = (Get-Pfa2Connection -Array $FlashArray | Where-Object { !($_.Volume
 ForEach ($PureConn in $PureConns)
 {
     $PureHost = (Get-Pfa2Host -Array $FlashArray | Where-Object { $_.Name -eq $PureConn.Host.Name })
-    $PureVol = (Get-Pfa2Volume -Array $FlashArray | Where-Object { $_.Name -eq $PureConn.Volume.Name })
-    $NewRow = "$($PureHost.Name),$($PureVol.Name),$($PureConn.Lun),$($PureHost.Iqns),$($PureHost.Wwns),$($PureVol.Provisioned/$1TB)"
+	$PureVol = (Get-Pfa2Volume -Array $FlashArray | Where-Object { $_.Name -eq $PureConn.Volume.Name })
+	
+	# Calculate and format Host Written Capacity, Volume Provisioned Capacity
+	$HostWrittenCapacity = [Math]::Round(($PureVol.Provisioned * (1-$PureVol.Space.ThinProvisioning))/1GB,2)
+	$VolumeProvisionedCapacity = [Math]::Round(($PureVol.Provisioned )/1TB,2)
+
+	$NewRow = "$($PureHost.Name),$($PureVol.Name),$($PureConn.Lun),$($PureHost.Iqns),$($PureHost.Wwns),"
+	$NewRow += "$($VolumeProvisionedCapacity),$($HostWrittenCapacity)"
     [void]$ConnDetails.Add($NewRow)
 }
 
