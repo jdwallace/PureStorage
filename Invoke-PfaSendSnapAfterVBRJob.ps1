a978bbcf87662ffb947904d99e6f5968ea0114a7
JD Wallace <jd@jdwallace.com>
2021-01-27 18:21:35 -0800

    Get all snaps for job and replicate.

--- Invoke-PfaSendSnapAfterVBRJob.ps1
+++ Invoke-PfaSendSnapAfterVBRJob.ps1
@@ -9,23 +9,12 @@
 	 Get Veeam Job Name - https://blog.mwpreston.net/2016/11/17/setting-yourself-up-for-success-with-veeam-pre-job-scripts/
 	===========================================================================
 	.DESCRIPTION
-    A Veeam VBR post-job script to be used with Pure FlashArray snapshot only job. Will replicate the new snapshot to a secondary FlashArray.
+	A Veeam VBR post-job script to be used with Pure FlashArray snapshot only job. Will replicate the new snapshot to a secondary FlashArray.
     Requires Purity//FA 6.1.0 or later.
 #>
 
 Add-PSSnapin VeeamPSSnapIn
 Import-Module PureStoragePowerShellSDK2
-
-# FlashArray OAuth2 Connection Variables for Primary FlashArray
-<#
-$arrayEndpoint = "fa-1.homelab.local"
-$arrayUsername = "vbr-server"
-$clientName = "VeeamPowerShell"
-$arrayIssuer = $Clientname
-$clientId = "5772f75a-df1c-4aca-843a-304abf875741"
-$keyId = "ed6d4780-61d9-4c5e-8ac0-39c48c9c85d9"
-$privateKeyFile = "C:\Users\Administrator.HOMELAB\.ssh\Pure¦fa-1.homelab.local¦vbr-server¦VeeamPowerShell¦5772f75a-df1c-4aca-843a-304abf875741¦ed6d4780-61d9-4c5e-8ac0-39c48c9c85d9¦private.pem"
-#>
 
 # FlashArray Credential for user/password authentication
 # Required since we're using Invoke-Pfa2CLICommand, will be replaced by OAuth2 in the future
@@ -36,6 +25,9 @@ $arrayCredential = New-Object -TypeName System.Management.Automation.PSCredentia
 # Destination Array - Must already be connected to Primary FlashArray
 $PfaSnapTarget = "FA-2"
 
+# Connect to FlashArray with user/password
+$FlashArray = Connect-Pfa2Array -EndPoint $arrayEndpoint -Credential $arrayCredential -IgnoreCertificateError
+
 # Get Veeam job name from parent process id
 <#
 $parentpid = (Get-WmiObject Win32_Process -Filter "processid='$pid'").parentprocessid.ToString()
@@ -43,25 +35,21 @@ $parentcmd = (Get-WmiObject Win32_Process -Filter "processid='$parentpid'").Comm
 $jobid = $parentcmd.split('" "')[16]
 $VbrJob = get-vbrjob | where-object { $_.Id -eq "$jobid" }
 #>
-
 # Get specific job (for development only)
 $VbrJob = Get-VBRJob | Where-Object { $_.Name -eq "Test - VMonFA" }
+# Get latest job Session (should be the session that called this script)
 $VbrSession = Get-VBRBackupSession | Where-Object {$_.jobId -eq $VbrJob.Id.Guid} | Sort-Object EndTimeUTC -Descending | Select-Object -First 1
-$VbrTaskSession = $VbrSession | Get-VBRTaskSession -Name "VMonFA"
 
+# Get all FlashArray snapshots created from this Job
+$jobNameInSnap = ".VEEAM-ProdSnap-" + $VbrJob.Name -replace ' ','-'
+$PfaSnapshots = Get-StoragePluginSnapshot | Where-Object { $_.Name -cmatch $jobNameInSnap }
 
-if (($VbrJob.info.TargetDir.ToString() -eq "Pure Storage Storage") -and $VbrSession.IsCompleted) {
-	$JobRepo = $VbrJob.FindTargetRepository()
+#Fore each Snapshot
+ForEach ($PfaSnapshot in $PfaSnapshots)
+{
+	#TODO Make sure to only replicate snap for latest session
 
-	# Connect to FlashArray via OAuth2 (Not used, unsupported by Invoke-Pfa2CLICommand)
-	# $FlashArray = Connect-Pfa2Array -Endpoint $arrayEndpoint -Username $arrayUsername -Issuer $arrayIssuer -ApiClientName $clientName -ClientId $clientId -KeyId $keyId -PrivateKeyFile $privateKeyFile -IgnoreCertificateError
-
-	# Connect to FlashArray with user/password
-	$FlashArray = Connect-Pfa2Array -EndPoint $arrayEndpoint -Credential $arrayCredential -IgnoreCertificateError
-
-	$jobSnap = "VMonFA.VEEAM-ProdSnap-Test---VMonFA-1B1096gCeP-wKmdZxLaE9nXsTA2m-frDgz" #TODO Figure out how to get snapshot name from Veeam job / Also might be multiple snaps
-	$snapSendCLI = "purevol send --to $PfaSnapTarget $jobSnap"
-
+	$snapSendCLI = "purevol send --to $PfaSnapTarget " + $PfaSnapshot.Name
 	# Invoke-Pfa2CLICommand to call "purevol send" to replicate the new snapshot
 	Invoke-Pfa2CLICommand -EndPoint $arrayEndpoint -Credential $arrayCredential -CommandText $snapSendCLI
 
