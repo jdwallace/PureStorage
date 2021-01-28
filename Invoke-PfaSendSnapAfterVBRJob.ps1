b1f81840d7b159a2e68dd9d9e6f844c62b63efb9
JD Wallace <jd@jdwallace.com>
2021-01-27 22:25:01 -0800

    Initial Working Version

--- Invoke-PfaSendSnapAfterVBRJob.ps1
+++ Invoke-PfaSendSnapAfterVBRJob.ps1
@@ -6,7 +6,7 @@
 	 Created by:   	JD Wallace
 	 Filename:     	Invoke-PfaSendSnapAfterVBRJob.ps1
 	 Acknowledgements: 
-	 Get Veeam Job Name - https://blog.mwpreston.net/2016/11/17/setting-yourself-up-for-success-with-veeam-pre-job-scripts/
+	 Get Veeam Job Name From Process ID - https://blog.mwpreston.net/2016/11/17/setting-yourself-up-for-success-with-veeam-pre-job-scripts/
 	===========================================================================
 	.DESCRIPTION
 	A Veeam VBR post-job script to be used with Pure FlashArray snapshot only job. Will replicate the new snapshot to a secondary FlashArray.
@@ -16,27 +16,28 @@
 Add-PSSnapin VeeamPSSnapIn
 Import-Module PureStoragePowerShellSDK2
 
-# FlashArray Credential for user/password authentication
+# Primary FlashArray Credential for user/password authentication
 # Required since we're using Invoke-Pfa2CLICommand, will be replaced by OAuth2 in the future
-$arrayUsername = "vbr-server"
-$arrayPassword = ConvertTo-SecureString "MyPassword" -AsPlainText -Force
+$arrayEndpoint = "fa-1.homelab.local"
+$arrayUsername = "pureuser"
+$arrayPassword = ConvertTo-SecureString "pureuser" -AsPlainText -Force
 $arrayCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $arrayUsername, $arrayPassword 
 
-# Destination Array - Must already be connected to Primary FlashArray
+# Destination FlashArray - Must already be connected to Primary FlashArray
 $PfaSnapTarget = "FA-2"
 
 # Connect to FlashArray with user/password
 $FlashArray = Connect-Pfa2Array -EndPoint $arrayEndpoint -Credential $arrayCredential -IgnoreCertificateError
 
 # Get Veeam job name from parent process id
-<#
 $parentpid = (Get-WmiObject Win32_Process -Filter "processid='$pid'").parentprocessid.ToString()
 $parentcmd = (Get-WmiObject Win32_Process -Filter "processid='$parentpid'").CommandLine
 $jobid = $parentcmd.split('" "')[16]
 $VbrJob = get-vbrjob | where-object { $_.Id -eq "$jobid" }
-#>
-# Get specific job (for development only)
-$VbrJob = Get-VBRJob | Where-Object { $_.Name -eq "Test - VMonFA" }
+
+# Get specific job (for testing only)
+# $VbrJob = Get-VBRJob | Where-Object { $_.Name -eq "FA Snapshot Job" }
+
 # Get latest job Session (should be the session that called this script)
 $VbrSession = Get-VBRBackupSession | Where-Object {$_.jobId -eq $VbrJob.Id.Guid} | Sort-Object EndTimeUTC -Descending | Select-Object -First 1
 
@@ -44,14 +45,17 @@ $VbrSession = Get-VBRBackupSession | Where-Object {$_.jobId -eq $VbrJob.Id.Guid}
 $jobNameInSnap = ".VEEAM-ProdSnap-" + $VbrJob.Name -replace ' ','-'
 $PfaSnapshots = Get-StoragePluginSnapshot | Where-Object { $_.Name -cmatch $jobNameInSnap }
 
-#For each Snapshot, check that it's from the latest session and replicate if so
+#For each FlashArray snapshot, check that it's from the latest session and replicate if so
 ForEach ($PfaSnapshot in $PfaSnapshots)
 {
-	#TODO Make sure to only replicate snap for latest session
-
-	$snapSendCLI = "purevol send --to $PfaSnapTarget " + $PfaSnapshot.Name
-	# Invoke-Pfa2CLICommand to call "purevol send" to replicate the new snapshot
-	Invoke-Pfa2CLICommand -EndPoint $arrayEndpoint -Credential $arrayCredential -CommandText $snapSendCLI
+	# If Snapshot was created after latest session start time, it must have been created during this session
+	If ($PfaSnapshot.CreationTimeSrv -gt $VbrSession.Info.CreationTime)
+	{
+		# Construct 'purevol send' command
+		$snapSendCLI = "purevol send --to $PfaSnapTarget " + $PfaSnapshot.Name
+		# Invoke-Pfa2CLICommand to call 'purevol send' to replicate the snapshot
+		Invoke-Pfa2CLICommand -EndPoint $arrayEndpoint -Credential $arrayCredential -CommandText $snapSendCLI
+	}
 }
 # Disconnect from FlashArray
 Disconnect-Pfa2Array -Array $FlashArray
--- README.md
+++ README.md
@@ -5,6 +5,5 @@ Coding samples and tools for use with Pure Storage solutions.
 ## Get-PfaConnectionDetails.ps1
 Output FlashArray connection details including Host and Volume names, LUN ID, IQN / WWN, Volume Provisioned Size, and Host Capacity Written. 
 
-## Invoke-PfaSendSnapAfterVBRJob.ps1
-**Work In Progress**  
+## Invoke-PfaSendSnapAfterVBRJob.ps1  
 A Veeam VBR post-job script to be used with Pure FlashArray snapshot only job. Will replicate the new snapshot to a secondary FlashArray. Requires Purity//FA 6.1.0 or later.
