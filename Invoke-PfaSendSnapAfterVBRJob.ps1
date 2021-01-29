32ebd16f762a9ad47d6f137ba266cf5bb83602b5
JD Wallace <jd@jdwallace.com>
2021-01-29 13:14:15 -0800

    Create "User Configuration" section to simplify

--- Invoke-PfaSendSnapAfterVBRJob.ps1
+++ Invoke-PfaSendSnapAfterVBRJob.ps1
@@ -14,36 +14,50 @@
 	Cleanup of unused snapshots on both source and target FlashArray not yet implemented.
 	Requires Purity//FA 6.1.0 or later.
 #>
+<#===========================================================================
+User Configuration - Customize these variables for your environment
+===========================================================================#>
+# Source FlashArray - Hosting VMFS Datastores being protected - Currently only 1 source FA supported per job
+$sourceArrayEndpoint = "fa-1.homelab.local"
+# Target FlashArray - Where will the Snapshots be replicated to?
+$targetArrayEndpoint = "fa-2.homelab.local"
+# User/Pass for FlashArray - Must exist on both FlashArrays. Must be storage-admin or higher
+# Required since we're using Invoke-Pfa2CLICommand, will be replaced by OAuth2 in the future
+$arrayUsername = "pureuser"
+$arrayPassword = "pureuser"
+# Name of Target FlashArray Host Group for Veeam Proxies
+# Must be pre-created and populated with Veeam Proxy servers in order for Veeam to scan replicas on target FlashArray and inventory VMs
+$PfaVeeamHostGroup = "Veeam-Proxies"
+# If DisableSourceRetention is TRUE, Snapshots will be removed from Source FlashArray after replication
+$DisableSourceRetention = $false
+# TestMode will use a specific Veeam Job Name, otherwise figure out Job Name based on which one launched the script
+$TestMode = $false
+$TestModeJobName = "FA Snapshot Job"
+<#===========================================================================
+End - User Configuration
+===========================================================================#>
 
 Add-PSSnapin VeeamPSSnapIn
 Import-Module PureStoragePowerShellSDK2
 
 # Primary FlashArray Credential for user/password authentication
-# Required since we're using Invoke-Pfa2CLICommand, will be replaced by OAuth2 in the future
-$sourceArrayEndpoint = "fa-1.homelab.local"
-$targetArrayEndpoint = "fa-2.homelab.local"
-$arrayUsername = "pureuser"
-$arrayPassword = ConvertTo-SecureString "pureuser" -AsPlainText -Force
-$arrayCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $arrayUsername, $arrayPassword 
-
-# Name of Target FlashArray Host Group for Veeam Proxies
-# Must be pre-created and populated with Veeam Proxy servers in order to scan replicas on target FlashArray
-$PfaVeeamHostGroup = "Veeam-Proxies"
-# If DisableSourceRetention is TRUE, delete local snapshot after replication
-$DisableSourceRetention = $true
+$arrayPasswordSS = ConvertTo-SecureString $arrayPassword -AsPlainText -Force
+$arrayCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $arrayUsername, $arrayPasswordSS 
 
 # Connect to FlashArrays with user/password
 $sourceFlashArray = Connect-Pfa2Array -EndPoint $sourceArrayEndpoint -Credential $arrayCredential -IgnoreCertificateError
 $targetFlashArray = Connect-Pfa2Array -EndPoint $targetArrayEndpoint -Credential $arrayCredential -IgnoreCertificateError
 
-# Get Veeam job name from parent process id
-$parentpid = (Get-WmiObject Win32_Process -Filter "processid='$pid'").parentprocessid.ToString()
-$parentcmd = (Get-WmiObject Win32_Process -Filter "processid='$parentpid'").CommandLine
-$jobid = $parentcmd.split('" "')[16]
-$VbrJob = get-vbrjob | where-object { $_.Id -eq "$jobid" }
-
-# Get specific job (for testing only)
-# $VbrJob = Get-VBRJob | Where-Object { $_.Name -eq "FA Snapshot Job" }
+if ($TestMode) {
+	# Get specific job
+	$VbrJob = Get-VBRJob | Where-Object { $_.Name -eq $TestModeJobName }
+}else {
+	# Get Veeam job name from parent process id
+	$parentpid = (Get-WmiObject Win32_Process -Filter "processid='$pid'").parentprocessid.ToString()
+	$parentcmd = (Get-WmiObject Win32_Process -Filter "processid='$parentpid'").CommandLine
+	$jobid = $parentcmd.split('" "')[16]
+	$VbrJob = get-vbrjob | where-object { $_.Id -eq "$jobid" }
+}
 
 # Get latest job Session (should be the session that called this script)
 $VbrSession = Get-VBRBackupSession | Where-Object {$_.jobId -eq $VbrJob.Id.Guid} | Sort-Object EndTimeUTC -Descending | Select-Object -First 1
@@ -54,11 +68,9 @@ $PfaSnapshots = Get-StoragePluginSnapshot | Where-Object { $_.Name -cmatch $jobN
 $PfaConfirmedSnapshots = New-Object -TypeName System.Collections.ArrayList
 
 #For each FlashArray snapshot, check that it's from the latest session and replicate if so
-ForEach ($PfaSnapshot in $PfaSnapshots)
-{
+ForEach ($PfaSnapshot in $PfaSnapshots) {
 	# If Snapshot was created after latest session start time, it must have been created during this session
-	If ($PfaSnapshot.CreationTimeSrv -gt $VbrSession.Info.CreationTime)
-	{
+	If ($PfaSnapshot.CreationTimeSrv -gt $VbrSession.Info.CreationTime) {
 		# Construct 'purevol send' command
 		$snapSendCLI = "purevol send --to " + $targetFlashArray.ArrayName.Split('.')[0] + " " + $PfaSnapshot.Name
 		# Invoke-Pfa2CLICommand to call 'purevol send' to replicate the snapshot
@@ -68,8 +80,7 @@ ForEach ($PfaSnapshot in $PfaSnapshots)
 	}
 }
 # For each FlashArray snapshot which was replicated, clone, snap, then clean up
-ForEach ($PfaConfirmedSnapshot in $PfaConfirmedSnapshots)
-{
+ForEach ($PfaConfirmedSnapshot in $PfaConfirmedSnapshots) {
 	# Wait for replication to complete
 	do {
 		Start-Sleep -s 5
@@ -83,8 +94,7 @@ ForEach ($PfaConfirmedSnapshot in $PfaConfirmedSnapshots)
 	$src = New-Pfa2ReferenceObject -Id $PfaSnapshotReplica.id -Name $PfaSnapshotReplica.Name
 	$PfaSnapshotClone = New-Pfa2Volume -Array $targetFlashArray -Overwrite $true -Source $src -Name ($PfaSnapshotReplica.Source.Name.Split(':')[1] + "-VEEAM-ProdSnap-Replica")
 	# Connect clone to Veeam Proxy Host Group
-	if ($null -eq (Get-Pfa2Connection -Array $targetFlashArray -HostGroupNames $PfaVeeamHostGroup -VolumeNames $PfaSnapshotClone.Name))
-	{
+	if ($null -eq (Get-Pfa2Connection -Array $targetFlashArray -HostGroupNames $PfaVeeamHostGroup -VolumeNames $PfaSnapshotClone.Name)) {
 		New-Pfa2Connection -Array $targetFlashArray -HostGroupNames $PfaVeeamHostGroup -VolumeNames $PfaSnapshotClone.Name
 	}
 	# Create Snapshot of Clone
@@ -94,11 +104,9 @@ ForEach ($PfaConfirmedSnapshot in $PfaConfirmedSnapshots)
 	$PfaReplicaCloneSnaps = Get-Pfa2VolumeSnapshot -Array $targetFlashArray -Destroyed $false -SourceNames $PfaSnapshotClone.Name | Sort-Object Created -Descending 
 
 	# See if number of snaps exceeds Job retention policy
-	if ($PfaReplicaCloneSnaps.Length -gt $VbrJob.Options.BackupStorageOptions.RetainCycles)
-	{
+	if ($PfaReplicaCloneSnaps.Length -gt $VbrJob.Options.BackupStorageOptions.RetainCycles) {
 		#Delete the excess snapshots
-		For ($i = $VbrJob.Options.BackupStorageOptions.RetainCycles; $i -lt $PfaReplicaCloneSnaps.Length; $i++ )
-		{
+		For ($i = $VbrJob.Options.BackupStorageOptions.RetainCycles; $i -lt $PfaReplicaCloneSnaps.Length; $i++ ) {
 			Remove-Pfa2VolumeSnapshot -Array $targetFlashArray -Name $PfaReplicaCloneSnaps[$i].Name
 		}
 	}
@@ -107,13 +115,11 @@ ForEach ($PfaConfirmedSnapshot in $PfaConfirmedSnapshots)
 	# Remove-Pfa2VolumeSnapshot -Array $targetFlashArray -Name $PfaSnapshotReplica.Name
 	
 	# Delete snapshot from Source FlashArray
-	if ($DisableSourceRetention)
-	{
+	if ($DisableSourceRetention) {
 		#TODO, Handle Baseline case
 		# Remove-Pfa2VolumeSnapshot -Array $sourceFlashArray -Name $PfaConfirmedSnapshot.Name
 	}
 }
-
 # Disconnect from FlashArray
 Disconnect-Pfa2Array -Array $sourceFlashArray
 Disconnect-Pfa2Array -Array $targetFlashArray
